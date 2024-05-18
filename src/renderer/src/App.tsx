82d4a4a2162f127114578f5a88c8c730e9bf6f5d
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-05-18 18:32:12 +0600

    update

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,4 @@
-// import companyLogo from './assets/media/logo.svg'
-// import electronLogo from './assets/media/second_user.webp'
-
-import { Button } from "./components/ui/button"
+import { Button } from './components/ui/button'
 
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
